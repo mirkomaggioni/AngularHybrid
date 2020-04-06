b6094c7b0db2dfc9ecf7b84896afa244be375914
Mirko Maggioni <mirko.maggioni@gmail.com>
2020-04-06 18:15:27 +0200

    odataResourceModule is refactored using typescript

---
+++ AngularHybridApplication.Web/app/main/shared/odataResource.ts
@@ -0,0 +1,29 @@
+﻿import { IRequestConfig } from "angular";
+
+export class OdataResource {
+  protected resourcePath: string;
+  constructor(private $http: ng.IHttpService) { }
+
+  protected list(query: string) {
+    return this.$http.get("/odata/" + this.resourcePath + query);
+  }
+  protected detail(id: string) {
+    return this.$http.get("/odata/" + this.resourcePath + "(" + id + ")");
+  }
+  protected create(entity: IResource) {
+    let req: IRequestConfig = { method: "POST", url: "/odata/" + this.resourcePath, data: entity, headers: { "Content-Type": "application/json" } };
+    return this.$http(req);
+  }
+  protected save(entity: IResource) {
+    let req: IRequestConfig = { method: "PATCH", url: "/odata/" + this.resourcePath + "(" + entity.Id + ")", data: entity, headers: { "Content-Type": "application/json" } };
+    return this.$http(req);
+  }
+  protected delete(id: string) {
+    return this.$http.delete("/odata/" + this.resourcePath + "(" + id + ")");
+  }
+}
+
+export interface IResource {
+  Id: any;
+  _originalResource: any;
+}
--- AngularHybridApplication.Web/app/main/shared/odataResourcesModule.ts
+++ AngularHybridApplication.Web/app/main/shared/odataResourcesModule.ts
@@ -1,184 +1,60 @@
-﻿(function (window, angular) {
-  'use-strict';
-  angular.module('odataResourcesModule', ['ui.router', 'ODataResources'])
-    .factory('odataResource', function ($odataresource, $http, $q) {
+﻿import * as angular from "angular";
+import { OdataResource, IResource } from './OdataResource';
 
-      function odataResource(serviceRootUrl, resourcePath, key) {
-        this.serviceRootUrl = serviceRootUrl;
-        this.resourcePath = resourcePath;
-        this.key = key;
+class OdataGenericResource extends OdataResource {
+  constructor($http: ng.IHttpService) { super($http); }
 
-        this._odataResource = $odataresource(serviceRootUrl + '/' + resourcePath, {}, {}, {
-          odatakey: key,
-          isodatav4: true
-        });
+  getOdataResource(resourcePath: string) {
+    this.resourcePath = resourcePath;
+    return this;
+  }
 
-        angular.extend(this._odataResource.prototype, {
-          '$patch': function () {
-            var defer = $q.defer();
+  get(id: string) {
+    return this.detail(id);
+  }
 
-            var req = {
-              method: 'PATCH',
-              url: serviceRootUrl + '/' + resourcePath + '(' + this.Id + ')',
-              data: this
-            };
+  save(resource: IResource) {
+    if (!resource._originalResource) {
+      return this.create(resource)
+    } else if (this.isChanged(resource)) {
+      var object = this.getObjectToUpdate(resource);
+      return this.save(object);
+    }
+  }
 
-            $http(req).then(function (data) {
-              defer.resolve(data);
-            }, function (error) {
-              defer.reject(error);
-            });
+  delete(id: string) {
+    return this.delete(id);
+  }
 
-            return defer.promise;
-          }
-        });
+  isChanged(resource: IResource) {
+    var isChanged = false;
+    for (var propertyName in resource) {
+      if (this.isEntityProperty(propertyName) && resource._originalResource[propertyName] !== resource[propertyName]) {
+        isChanged = true;
       }
+    }
 
-      odataResource.prototype.getResource = function () {
-        return this._odataResource.odata();
-      };
+    return isChanged;
+  };
 
-      odataResource.prototype.new = function () {
-        return new this._odataResource();
-      };
+  private isEntityProperty(propertyName: string) {
+    return (propertyName !== '$promise' && propertyName !== '_originalResource');
+  }
 
-      odataResource.prototype.get = function (id) {
-        var defer = $q.defer();
 
-        this._odataResource.odata().get(id, function (data) {
-          data._originalResource = angular.copy(data);
-          defer.resolve(data);
-        }, function (error) {
-          defer.reject(error);
-        });
+  private getObjectToUpdate(resource: IResource) {
+    var object = new object();
+    object['Id'] = resource.Id;
 
-        return defer.promise;
-      };
-
-      odataResource.prototype.add = function (resource) {
-        var defer = $q.defer();
-
-        resource.$save(function (data) {
-          data._originalResource = angular.copy(data);
-          defer.resolve(data);
-        }, function (error) {
-          defer.reject(error);
-        });
-
-        return defer.promise;
-      };
-
-      odataResource.prototype.update = function (resource) {
-        var defer = $q.defer();
-
-        var self = this;
-        resource.$patch().then(function () {
-          self.get(resource[self.key]).then(function (data) {
-            defer.resolve(data);
-          });
-        }, function (error) {
-          defer.reject(error);
-        });
-
-        return defer.promise;
-      };
-
-      odataResource.prototype.delete = function (resource) {
-        var defer = $q.defer();
-
-        resource.$delete(function (data) {
-          defer.resolve(data);
-        }, function (error) {
-          defer.reject(error);
-        });
-
-        return defer.promise;
-      };
-
-      odataResource.prototype.resource = function () {
-        return this._odataResource;
-      };
-
-      return odataResource;
-    })
-
-    .factory('odataGenericResource', function ($q, odataResource) {
-
-      function odataGenericResource(serviceRootUrl, resourcePath, key) {
-        this.odataResource = new odataResource(serviceRootUrl, resourcePath, key);
+    for (var propertyName in resource) {
+      if (this.isEntityProperty(propertyName) && resource._originalResource[propertyName] !== resource[propertyName]) {
+        object[propertyName] = resource[propertyName];
       }
+    }
 
-      var isEntityProperty = function (propertyName) {
-        return (propertyName !== '$promise' && propertyName !== '_originalResource');
-      };
+    return object;
+  };
+}
 
-      odataGenericResource.prototype.getOdataResource = function () {
-        return this.odataResource.getResource();
-      };
-
-      odataGenericResource.prototype.get = function (id) {
-        if (id === '') {
-          var defer = $q.defer();
-          defer.resolve(this.odataResource.new());
-
-          return defer.promise;
-        } else {
-          return this.odataResource.get(id);
-        }
-      };
-
-      odataGenericResource.prototype.save = function (resource) {
-        if (!resource._originalResource) {
-          return this.odataResource.add(resource);
-        } else if (this.isChanged(resource)) {
-          var object = this.getObjectToUpdate(resource);
-          return this.odataResource.update(object);
-        } else {
-          var defer = $q.defer();
-          defer.resolve(resource);
-
-          return defer.promise;
-        }
-      };
-
-      odataGenericResource.prototype.delete = function (resource) {
-        return this.odataResource.delete(resource);
-      };
-
-      odataGenericResource.prototype.isChanged = function (resource) {
-        var isChanged = false;
-        for (var propertyName in resource) {
-          if (isEntityProperty(propertyName) && resource._originalResource[propertyName] !== resource[propertyName]) {
-            isChanged = true;
-          }
-        }
-
-        return isChanged;
-      };
-
-      odataGenericResource.prototype.getObjectToUpdate = function (resource) {
-        var object = this.odataResource.new();
-        object[this.odataResource.key] = resource[this.odataResource.key];
-
-        for (var propertyName in resource) {
-          if (isEntityProperty(propertyName) && resource._originalResource[propertyName] !== resource[propertyName]) {
-            object[propertyName] = resource[propertyName];
-          }
-        }
-
-        return object;
-      };
-
-      odataGenericResource.prototype.restore = function (resource) {
-        for (var propertyName in resource) {
-          if (isEntityProperty(propertyName) && resource._originalResource[propertyName] !== resource[propertyName]) {
-            resource[propertyName] = resource._originalResource[propertyName];
-          }
-        }
-
-        return resource;
-      };
-
-      return odataGenericResource;
-    });
-})(window, window.angular);
+export const serviziModule = angular.module('odataResourcesModule', ['ui.router'])
+  .factory('odataGenericResource', OdataGenericResource);
