f38a7584f69e4c7bd1edd5ef556aaf446e2d70ff
Mirko Maggioni <mirko.maggioni@gmail.com>
2020-04-06 15:57:35 +0200

    uiModule is refactored using typescript

--- AngularHybridApplication.Web/app/main/shared/uiModule.ts
+++ AngularHybridApplication.Web/app/main/shared/uiModule.ts
@@ -1,18 +1,17 @@
-﻿(function (window, angular) {
-  'use-strict';
-  angular.module('uiModule', [])
-    .directive('uiSelect',
-      function () {
-        return {
-          restrict: 'E',
-          scope: {
-            id: '@',
-            name: '@',
-            options: '=',
-            selectedOption: '=',
-            isRequired: '@'
-          },
-          templateUrl: 'app/main/shared/ui-select.html'
-        };
-      });
-})(window, window.angular);
+﻿import * as angular from 'angular';
+
+export const uiModule = angular.module('uiModule', [])
+  .directive('uiSelect',
+    function () {
+      return {
+        restrict: 'E',
+        scope: {
+          id: '@',
+          name: '@',
+          options: '=',
+          selectedOption: '=',
+          isRequired: '@'
+        },
+        templateUrl: 'app/main/shared/ui-select.html'
+      };
+    });
