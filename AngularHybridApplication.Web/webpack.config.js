a7452457601a20357446f1b083ab7ed4fc7edd47
Mirko Maggioni <mirko.maggioni@gmail.com>
2020-04-09 14:04:52 +0200

    webpack configuration is added

---
+++ AngularHybridApplication.Web/webpack.config.js
@@ -0,0 +1,49 @@
+﻿var webpack = require('webpack');
+
+var AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
+var path = require('path');
+
+var DEV_SERVER = process.argv[1].indexOf('webpack-dev-server') !== -1;
+var DEV = DEV_SERVER || process.env.DEV;
+
+module.exports = {
+  mode: DEV ? 'development' : 'production',
+  entry: {
+    "hybridapp": "./app/bootstrap.ts"
+  },
+
+  devtool: DEV ? 'eval' : 'source-map',
+
+  output: {
+    path: path.join(__dirname, "_bundles"),
+    publicPath: '_bundles/',
+    filename: "[name].js"
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+
+  optimization: {
+    splitChunks: { chunks: 'all', },
+  },
+
+  plugins: [
+    new AngularCompilerPlugin({
+      "tsConfigPath": 'tsconfig.json',
+      "mainPath": 'app/bootstrap.ts',
+      "sourceMap": true
+    })
+  ],
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: ["source-map-loader"], enforce: 'pre' },
+      { test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/, use: ["@ngtools/webpack"] }
+    ]
+  },
+
+  externals: {
+    angular: 'angular'
+  }
+};
