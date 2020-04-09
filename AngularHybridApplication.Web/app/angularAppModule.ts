707de922539ca32e8d37963c5f7ff3cfee5edcdf
Mirko Maggioni <mirko.maggioni@gmail.com>
2020-04-09 14:19:47 +0200

    angular module is added

---
+++ AngularHybridApplication.Web/app/angularAppModule.ts
@@ -0,0 +1,20 @@
+﻿import { UpgradeModule } from '@angular/upgrade/static';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UIRouterUpgradeModule } from '@uirouter/angular-hybrid';
+import angularjsAppModule from './angularjsAppModule'
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    UpgradeModule,
+    UIRouterUpgradeModule.forRoot()
+  ]
+})
+export class BootstrapModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    console.log("Angularjs Module Launch");
+    this.upgrade.bootstrap(document.body, [angularjsAppModule]);
+  }
+}
