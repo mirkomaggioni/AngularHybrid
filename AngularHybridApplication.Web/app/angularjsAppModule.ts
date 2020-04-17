5aa1a34c6d069be3e7dda6cf677b6cbc3a079847
Mirko Maggioni <mirko.maggioni@gmail.com>
2020-04-17 17:11:47 +0200

    angular hybrid injection error is fixed

--- AngularHybridApplication.Web/app/angularjsAppModule.ts
+++ AngularHybridApplication.Web/app/angularjsAppModule.ts
@@ -3,8 +3,9 @@ import { mainModule } from './main/mainModule';
 import { blogsModule } from './main/blogs/blogsModule';
 import { postsModule } from './main/posts/postsModule';
 import { uiModule } from './main/shared/uiModule';
+import { upgradeModule } from '@uirouter/angular-hybrid';
 
-export default angular.module('angularjsApp', ['ui.router', mainModule.name, blogsModule.name, postsModule.name, uiModule.name])
+export default angular.module('angularjsApp', ['ui.router', mainModule.name, blogsModule.name, postsModule.name, uiModule.name, upgradeModule.name])
   .config(function ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/home/blogs');
     $locationProvider.hashPrefix('');
